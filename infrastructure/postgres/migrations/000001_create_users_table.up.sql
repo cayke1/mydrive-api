2ba53ba4d51ca1b0e8b124b0b6bafe996588b8c9
Cayke Daniel <caykedanielcd@gmail.com>
2026-09-10 18:49:22 -0300

    chore: update users migration

--- infrastructure/postgres/migrations/000001_create_users_table.up.sql
+++ infrastructure/postgres/migrations/000001_create_users_table.up.sql
@@ -1,5 +1,9 @@
 CREATE TABLE users (
     id UUID PRIMARY KEY,
     email VARCHAR(100) NOT NULL UNIQUE,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    session_token VARCHAR(255),
+    CSRF_token VARCHAR(255)
 );
